070cfe1ccb466bcbe6755b8853638fa72cef82df
Thomas Derudder <thomasderudder@sfr.fr>
2025-04-10 14:33:51 +0200

    :sparkles: feat: update pre-commit

---
+++ test/test_hello_world.py
@@ -0,0 +1,7 @@
+# test_hello_world.py
+
+
+def test_hello_world():
+    # Simple test to print "Hello, World!" and assert that it passes
+    print("Hello, World!")
+    assert True  # This will always pass
